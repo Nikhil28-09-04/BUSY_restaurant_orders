2f9989b1ed4429a1016aa2bc31e82312305b30a6
NIKHIL TIWARI <66139927+Nikhil28-09-04@users.noreply.github.com>
2026-09-05 15:42:17 +0530

    Updated index.js

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "http://https://busy-restaurant-orders-p5nveyrhc-tests-projects-d49f9ef5.vercel.app",
+    origin: "https://busy-restaurant-orders-p5nveyrhc-tests-projects-d49f9ef5.vercel.app",
     credentials: true,
   }),
 );
